b7fdd6a6c7aeba8d176e5d9e5e81fa8dbeea32f7
unknown <abhishekraj2447@gmail.com>
2025-09-02 16:01:38 +0530

    fixing routing error

--- app/stock/[symbol]/page.jsx
+++ app/stock/[symbol]/page.jsx
@@ -8,6 +8,8 @@ import GraphComp from "@/app/components/GraphComp";
 export default function StockPage() {
   const { symbol } = useParams();
   const [data, setData] = useState(null);
+  const [text, setText] = useState(false);
+  const [graph, setGraph] = useState(false);
 
   useEffect(() => {
     if (!symbol) return;
@@ -28,10 +30,27 @@ export default function StockPage() {
 
   return (
     <>
+ 
+
      <div className="min-h-screen bg-gray-950 text-white p-4 sm:p-6">
       <h1 className="text-2xl sm:text-3xl font-bold mb-4">
         selected_Stock: {symbol}
       </h1>
+        <div className="mb-4 flex justify-center gap-4 mt-4">
+  <button
+    className="bg-green-800 text-white px-5 py-2 rounded-lg shadow-md hover:bg-green-700 transition duration-200"
+    onClick={() => setText(!text)}
+  >
+    Show_Me_Text
+  </button>
+
+  <button
+    className="bg-green-800 text-white px-5 py-2 rounded-lg shadow-md hover:bg-green-700 transition duration-200"
+    onClick={() => setGraph(!graph)}
+  >
+   Show_Me_Graph
+  </button>
+</div>
 
       {!data && <div className="flex justify-center items-center h-32">
   <div className="w-10 h-10 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
